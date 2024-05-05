53930728c7a38b014e7eca5c51053c2c7d6391f5
Leon Chow <leon.chow1@ontariotechu.net>
2024-05-05 17:39:49 -0400

    init commit, add main routing pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CollectionComponent } from './components/collection/collection.component';
+import { DetailsComponent } from './components/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "characters", component: CollectionComponent },
+  { path: "characters/:id", component: DetailsComponent}
+];
