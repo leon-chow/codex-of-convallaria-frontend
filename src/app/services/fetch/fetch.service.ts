31de236a25c74d2ad4ccd7f3e7b626986a9719cb
Leon Chow <leon.chow1@ontariotechu.net>
2024-06-04 13:09:44 -0400

    feat: add fetch service, refactor character servIce

--- src/app/services/fetch/fetch.service.ts
+++ src/app/services/fetch/fetch.service.ts
@@ -1,25 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { API_URL } from '../../utils/constants';
-import { of } from 'rxjs';
 import { ICharacter } from '../../models/character';
-import { FetchService } from '../fetch/fetch.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CharactersService {
+export class FetchService {
   token = "qhp9Fi53DlPzz7U3kD0AfkQVrYd1Mz4kT2QC8IFIVDnefPVfm5r35QALs3Wjdb6k";
   header = new HttpHeaders({ 
     'Authorization': `Bearer ${this.token}`, 
   });
-
-  constructor(private http: HttpClient, private fetchService: FetchService) { 
+  constructor(private http: HttpClient) { 
 
   }
 
-  getCharacters() {
-    const path = `/characters`;
-    return this.fetchService.get(path); 
+  get(path?: string, headers?: HttpHeaders) {
+    const constructedUrl = API_URL + path;
+    return this.http.get<ICharacter[]>(constructedUrl, {headers: this.header, ...headers});
   }
 }
