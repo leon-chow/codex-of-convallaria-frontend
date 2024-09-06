f1698a664ee68f28fd13f97f9f728547edb4f507
Leon Chow <leon.chow1@ontariotechu.net>
2024-09-06 17:26:28 -0400

    Merge branch 'release' of https://github.com/leon-chow/codex-of-convallaria-frontend into release

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { DetailsComponent } from './components/details/details.component';
 import { RecruitComponent } from './components/recruit/recruit.component';
 
 export const routes: Routes = [
-  { path: "", component: HomeComponent },
+  { path: "", component: RecruitComponent },
   { path: "characters", component: CollectionComponent, data: {type: "characters"}},
   { path: "characters/:id", component: DetailsComponent },
-  { path: "recruit", component: RecruitComponent }
+  { path: "#recruit", component: RecruitComponent }
 ];
